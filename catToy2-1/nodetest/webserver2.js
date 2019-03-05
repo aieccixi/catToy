3223fabbf4539168209be28648cbc4a876a570f2
aieccixi <aieccixi@gmail.com>
2019-03-05 15:47:53 -0800

    Updated to handle Right press
    Does not work, needs work on io.sockets.on line 30-50

--- catToy2-1/nodetest/webserver2.js
+++ catToy2-1/nodetest/webserver2.js
@@ -8,34 +8,49 @@ var led = 0;
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index2.html', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
+	fs.readFile(__dirname + '/public/index2.html', function(err, data) { //read file index.html in public folder
+		if (err) {
+			res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+			return res.end("404 Not Found");
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+		res.write(data); //write data from index.html
+		return res.end();
+	});
 }
 
-io.sockets.on('connection', function (socket) {// WebSocket Connection
-  var lightvalue = 0; //static variable for current status
-  socket.on('spaceBar', function(data) { //get light switch status from client
-    lightvalue = data;
-	console.log('This is server side');
-	console.log(lightvalue);
-    if (lightvalue) { //only change LED if status has changed
-		console.log('Server thinks True!');
-		led = 1;
-      //LED.writeSync(lightvalue); //turn LED on or off
-    }
-  });
-});
+/*
+NEEDS WORK!!!!!
 
-process.on('SIGINT', function () { //on ctrl+c
-  //LED.writeSync(0); // Turn LED off
-  //LED.unexport(); // Unexport LED GPIO to free resources
-  //pushButton.unexport(); // Unexport Button GPIO to free resources
-  process.exit(); //exit completely
+spacebar needs to be detected first and then right gets detected
+then right and space send twice maybe something to do with keyDown/UpHandlers
+
+
+*/
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+	var spacePressed = false; //static variable for current status
+	var rightPressed = false;
+	socket.on('spaceBar', function(data) { //get light switch status from client
+		spacePressed = data;
+		console.log('This is server side');
+		console.log(spacePressed);
+		if (spacePressed) { //only change LED if status has changed
+			led = 1;
+			//LED.writeSync(lightvalue); //turn LED on or off
+		}
+	socket.on('right', function(data) {
+		rightPressed = data;
+		if (rightPressed){
+			console.log('This is server side RIGHT');
+			console.log(rightPressed);
+		}
+		});
+
+	});
 });
+process.on('SIGINT', function () { //on ctrl+c
+	//LED.writeSync(0); // Turn LED off
+	//LED.unexport(); // Unexport LED GPIO to free resources
+	//pushButton.unexport(); // Unexport Button GPIO to free resources
+	process.exit(); //exit completely
+})
